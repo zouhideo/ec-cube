19092d34f36aaceedf9a5de2b625a5622792882b
zouhideo <zouhideo@gmail.com>
2019-06-14 17:39:29 +0900

    test

--- docker/eccube/entrypoint.sh
+++ docker/eccube/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
+# www-dataのUIDを変更する
+if [ -n "${HOST_UID}" -a "${HOST_UID}" != "$(id -u www-data)" ]; then
+    usermod -u "${HOST_UID}" www-data
+fi
+
 # リモートデバッグの設定は環境変数だけでは対応できないため動的に追記
 xdebug=$(cat << EOS
 xdebug.remote_host=$(ip route | awk 'NR==1 {print $3}')
@@ -10,11 +15,13 @@ EOS
 echo "${xdebug}" >> /usr/local/etc/php/conf.d/shop.ini
 
 if [ -e "composer.json" ]; then
-    composer install
+    #composer install
+    sudo -u www-data composer install
 fi
 
 if [ -e "package.json" ]; then
-    npm install
+    #npm install
+    sudo -u www-data npm install
 fi
 
 if [ "${1#-}" != "$1" ]; then
